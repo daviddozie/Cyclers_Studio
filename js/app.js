f2afb81a32a80167e7f31e012b7f50c7b3d7fe5a
David Mgbede <davidmgbede75@gmail.com>
2023-10-12 16:09:10 +0100

    updates

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     speed: '1000',
-    
+
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
@@ -40,3 +40,23 @@ const swiper = new Swiper('.swiper', {
         }
     }
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
